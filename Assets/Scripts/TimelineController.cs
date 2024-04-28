c7dccaa1e37613a72bedc59681c8fb6dcc4cdc9c
Siyuan Peng <peng2000@umd.edu>
2024-04-28 12:51:40 -0400

    Added timeline control

---
+++ Assets/Scripts/TimelineController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TimelineController : MonoBehaviour
+{
+    public Boolean enbaleLogger = false;
+    private float startTime;
+    private int UID;
+    // Start is called before the first frame update
+    private Boolean isStationaryLoaded = false;
+    private Boolean isMovingLoaded = false;
+
+    private void LoadStationaryScene()
+    {
+        SceneManager.LoadScene("IllusionStationaryScene");
+
+    }
+    private void LoadMovingScene()
+    {
+        SceneManager.LoadScene("IllusionMovingScene");
+    }
+    public void GetNextScene()
+    {
+        if (!isStationaryLoaded && !isMovingLoaded)
+        {
+            // if both scenes are not loaded, randomly load one of them
+            if (UnityEngine.Random.Range(0, 2) == 0)
+            {
+                LoadStationaryScene();
+                isStationaryLoaded = true;
+            }
+            else
+            {
+                LoadMovingScene();
+                isMovingLoaded = true;
+            }
+        }
+        else if (isStationaryLoaded && !isMovingLoaded)
+        {
+            LoadMovingScene();
+            isMovingLoaded = true;
+        }
+        else if (!isStationaryLoaded && isMovingLoaded)
+        {
+            LoadStationaryScene();
+            isStationaryLoaded = true;
+        }
+        else
+        {
+            Debug.LogError("Expereinment is over, both scenes are loaded.");
+        }
+
+    }
+    void Start()
+    {
+        startTime = Time.time;
+        // randomly generate a UID
+        UID = UnityEngine.Random.Range(0, 10000);
+        if (enbaleLogger)
+        {
+            Debug.Log(" == UID: " + UID + " == ");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
